91a142028b4c49a4eadbff44030b86f6b0f4dea4
Freddy <ferreirafreddy39@gmail.com>
2023-01-27 11:29:56 -0300

    iniciode la app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a Angular!';
+  curso: string = 'el curso es con angular 7';
+  alumno: string = 'freddy ferreira';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FooterComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+    ]
 })
 export class AppModule { }
