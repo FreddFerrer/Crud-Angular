c9658aab9c0012b8ab4b6f8dfbc0920c4f2c7bda
FreddyWhitDoubleD <ferreirafreddy39@gmail.com>
2023-02-08 17:37:06 -0300

    agregando components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        FooterComponent
+        FooterComponent,
+        DirectivaComponent
     ],
     providers: [],
     bootstrap: [AppComponent],
