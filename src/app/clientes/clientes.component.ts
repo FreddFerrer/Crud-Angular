2e872189f9ed1e96f53b0c7d27be932f5de31fd8
Freddy <ferreirafreddy39@gmail.com>
2023-03-01 20:57:26 -0300

    ejer terminado

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -3,6 +3,7 @@ import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { CLIENTES } from './clientes.json';
 import { Observable, of } from 'rxjs';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -21,6 +22,37 @@ export class ClientesComponent{
     );
   }
 
-
+  delete(cliente: Cliente): void{
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: `Seguro que desea borrar el cliente ${cliente.nombre} ${cliente.apellido}? `,
+      text: "No podrá volver atras",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, estoy seguro!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter( cli => cli !== cliente)
+            swalWithBootstrapButtons.fire(
+              'Cliente eliminado!',
+              'success'
+            )
+          }
+        )
+        
+      } 
+    })
+  }
 
 }
--- src/app/clientes/formulario/formulario.component.ts
+++ src/app/clientes/formulario/formulario.component.ts
@@ -15,13 +15,15 @@ export class FormularioComponent implements OnInit{
     this.cargarCliente()
   }
 
+  public cliente:Cliente = new Cliente();
+  public errores: string[];
+  public titulo:string = "Crear Cliente!";
+
   constructor(private clienteService: ClienteService,
     private router: Router,
     private activatedRoute: ActivatedRoute){};  
 
-  public cliente:Cliente = new Cliente();
-
-  public titulo:string = "Crear Cliente!";
+  
 
   public cargarCliente(): void {
     this.activatedRoute.params.subscribe((params) =>{ 
@@ -33,15 +35,33 @@ export class FormularioComponent implements OnInit{
     )
   }
 
+  //usamos el objeto para mostrar el mensaje
   public create(): void{
     this.clienteService.create(this.cliente).subscribe(
       cliente => { 
         this.router.navigate(['/clientes'])
-        swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con exito`, 'success')
+        swal.fire('Nuevo cliente', `${cliente.nombre}`, 'success')
+      },
+      err => {
+        //toma el error desde el backend
+        this.errores = err.error.errores as string[]
       }
     )
   }
 
+  //otra forma de mostrar el mensaje: usando el mensaje de la bd
+  public update(): void{
+    this.clienteService.updateCliente(this.cliente).subscribe(
+      json => {
+        this.router.navigate(['/clientes'])
+        swal.fire('Cliente actualziado', `${json.mensaje}`, 'success')
+      },
+      err => {
+        //toma el error desde el backend
+        this.errores = err.error.errores as string[]
+      }
+    )
+  }
   
 
 }
